55bc176d62a76d84e2f1c387561a511be65207c1
Mustafa Shahriar Hazary <45937799+shahriar30j@users.noreply.github.com>
2019-10-08 04:29:48 +0600

    Section 12 complete
    Shopping Cart up to cart and Order (Complete)

--- controllers/shop.js
+++ controllers/shop.js
@@ -95,39 +95,17 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
   let fetchedCart;
   req.user
-    .getCart()
-    .then(cart => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then(products => {
-      return req.user
-        .createOrder()
-        .then(order => {
-          // order.addProducts(products, {through:{ quantity }});
-          return order.addProducts(
-            products.map(product => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            }));
-        })
-        .catch(err => console.log(err));
-      // console.log(products);
-    })
+    .addOrder()
     .then(result => {
-      return fetchedCart.setProducts(null);
-    })
-    .then(result => {
-
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
 }
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders({ include: ['products'] })
+  req.user
+    .getOrders()
     .then(orders => {
-      // console.log(orders);
       res.render('shop/orders', {
         path: '/orders',
         pageTitle: 'Your Orders',
