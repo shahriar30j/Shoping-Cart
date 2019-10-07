9625f7c30cf584a6882f6afca128d58c3a2364c0
Mustafa Shahriar Hazary <45937799+shahriar30j@users.noreply.github.com>
2019-10-08 01:30:41 +0600

    shopping Cart
    Create Cart Add Delete  Show products/ietms in Cart

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById("5d9b1c5c3a7ff43088f731bc")
         .then(user => {
-            req.user = user;
+            req.user = new User(user.name, user.email, user.cart, user._id );
             next();
         })
         .catch(err => console.log(err));
